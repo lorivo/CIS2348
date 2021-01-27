a14378e71b33c32cca0b3c134c34329ab5d476f2
lorivo <77751574+lorivo@users.noreply.github.com>
2021-01-27 14:51:57 -0600

    Add files via upload
    homework 1

---
+++ 1.12 lab.py
@@ -0,0 +1,4 @@
+userNum = int(input())
+userNumSquared = userNum **2 # Bug here; fix it when instructed
+
+print(userNumSquared, end="\n") # Output formatting issue here; fix it when instructed
---
+++ homework1birthday.py
@@ -0,0 +1,7 @@
+#Lori Vo
+#1852113
+
+print("Birthday Calculator ")
+print("Current day")
+print("Month:")
+print("Day:")
