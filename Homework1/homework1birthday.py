0128885695e2506cb9343c143804992fc574feeb
lorivo <77751574+lorivo@users.noreply.github.com>
2021-01-28 12:50:59 -0600

    Add files via upload

---
+++ Homework1/homework1birthday.py
@@ -0,0 +1,27 @@
+#Lori Vo
+#Homework 1
+#1852113
+
+import time
+
+print("Birthday Calculator ")
+print("Current day")
+givenmonth=int(input("Month: "))
+givendate=int(input("Day: "))
+givenyear=int(input("Year: "))
+
+print("Birthday")
+birthmonth=int(input("Month: "))
+birthday=int(input("Day: "))
+birthyear=int(input("Year: "))
+
+if givenmonth>=birthmonth:
+    if givendate>=birthday:
+        print("You are",(givenyear-birthyear),"years old.")
+    else:
+        print("You are", (givenyear - birthyear)-1, "years old.")
+else:
+    print("You are",(givenyear-birthyear)-1,"years old.")
+
+if (givenmonth==birthmonth)and(givendate==birthday):
+    print("Happy Birthday!")
