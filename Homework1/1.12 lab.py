9264fd5ca0695a1b5ff38a4315eafa8e1883dd22
lorivo <77751574+lorivo@users.noreply.github.com>
2021-02-03 13:40:59 -0600

    Add files via upload

---
+++ Homework1/1.12 lab.py
@@ -0,0 +1,7 @@
+#Lori Vo
+#1852113
+
+userNum = int(input())
+userNumSquared = userNum **2 # Bug here; fix it when instructed
+
+print(userNumSquared, end="\n") # Output formatting issue here; fix it when instructed
