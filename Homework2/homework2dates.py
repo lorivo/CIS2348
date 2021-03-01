e4203ee9fdcf85aeb4424ad0024a557356dc4245
lorivo <77751574+lorivo@users.noreply.github.com>
2021-03-01 17:34:55 -0600

    Add files via upload

--- Homework2/homework2dates.py
+++ Homework2/homework2dates.py
@@ -16,43 +16,43 @@ today_month = today.month
 
 # part b which just requires reading given file
 filedates = open('inputDates.txt')
-date_filecontents = filedates.read()
+date_filecontents = filedates.readlines()
 filedates.close()
 
-givenstring = input()
+print(date_filecontents)
 
-while givenstring != '-1':
-    if (givenstring[-5:-4] == ' ') and (givenstring[-6:-5] == ',') and (givenstring.count(' ') == 2) \
-            and givenstring[:givenstring.find(' ')] in months2num.keys():
-        # requirements: needs comma and space before year, space before date, month should be in dictionary, 2 spaces
-        space1 = givenstring.find(' ')
-        # find first space
-        space2 = givenstring.rfind(' ')
-        # find second space
-        month = givenstring[0: space1]
-        # month string is from beginning to first space
-        day = givenstring[space1+1: space2-1]
-        # day string is from first space comma
-        year = givenstring[space2+1:]
-        # year is from second space to end
-        num_month = months2num[month]
-        # converts given month to int
-        if int(year) < today_year:
-            # checks if year given is before today's year, if yes then output
-            wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
-            print(wantedoutput)
-        elif int(year) == today_year:
-            # if not ^, givenyear is same as today's year, check to see if given month is before
-            if num_month < today_month:
+for n in (date_filecontents):
+    if n != '-1':
+        if (n[-6:-5] == ' ') and (n[-7:-6] == ',') and (n.count(' ') == 2) \
+                and n[:n.find(' ')] in months2num.keys():
+            # requirements: needs comma and space before year, space before date, month should be in dictionary, 2 spaces
+            space1 = (n.find(' '))
+            # find first space
+            space2 = (n.rfind(' '))
+            # find second space
+            month = n[0: space1]
+            # month string is from beginning to first space
+            day = n[space1+1: space2-1]
+            # day string is from first space comma
+            year = n[space2+1:]
+            # year is from second space to end
+            num_month = months2num[month]
+            # converts given month to int
+            if int(year) < today_year:
+                # checks if year given is before today's year, if yes then output
                 wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
                 print(wantedoutput)
-            elif num_month == today_month:
-                # if not ^, if given month = today's monday then check if given day is before today or same day
-                if int(day) <= today_day:
+            elif int(year) == today_year:
+                # if not ^, givenyear is same as today's year, check to see if given month is before
+                if num_month < today_month:
                     wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
                     print(wantedoutput)
-        givenstring = input()
-        # input can repeat loop or exit with -1
-    else:
-        givenstring = input()
-        # repeat loop or -1
+                elif num_month == today_month:
+                    # if not ^, if given month = today's monday then check if given day is before today or same day
+                    if int(day) <= today_day:
+                        wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
+                        print(wantedoutput)
+            # input can repeat loop or exit with -1
+        else:
+            continue
+            # repeat loop or -1
