08bc42694e91cd8b21a82cc83216bbe9f0da4292
lorivo <77751574+lorivo@users.noreply.github.com>
2021-03-01 17:44:39 -0600

    Add files via upload

--- Homework2/homework2dates.py
+++ Homework2/homework2dates.py
@@ -17,15 +17,19 @@ today_month = today.month
 # part b which just requires reading given file
 filedates = open('inputDates.txt')
 date_filecontents = filedates.readlines()
+# reads each line and puts it into a list
 filedates.close()
 
-print(date_filecontents)
+# part c, creating new file to write into
+f = open('parsedDates.txt','w')
 
-for n in (date_filecontents):
+for n in date_filecontents:
+    # n is each part of the list
     if n != '-1':
         if (n[-6:-5] == ' ') and (n[-7:-6] == ',') and (n.count(' ') == 2) \
                 and n[:n.find(' ')] in months2num.keys():
-            # requirements: needs comma and space before year, space before date, month should be in dictionary, 2 spaces
+            # requirements: needs comma and space before year, space before date,
+            # month should be in dictionary, 2 spaces
             space1 = (n.find(' '))
             # find first space
             space2 = (n.rfind(' '))
@@ -41,18 +45,21 @@ for n in (date_filecontents):
             if int(year) < today_year:
                 # checks if year given is before today's year, if yes then output
                 wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
-                print(wantedoutput)
+                # the following line writes the output into the file
+                f.write(wantedoutput)
             elif int(year) == today_year:
                 # if not ^, givenyear is same as today's year, check to see if given month is before
                 if num_month < today_month:
                     wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
-                    print(wantedoutput)
+                    f.write(wantedoutput)
                 elif num_month == today_month:
                     # if not ^, if given month = today's monday then check if given day is before today or same day
                     if int(day) <= today_day:
                         wantedoutput = str(num_month) + '/' + str(day) + '/' + str(year)
-                        print(wantedoutput)
+                        f.write(wantedoutput)
             # input can repeat loop or exit with -1
         else:
             continue
             # repeat loop or -1
+# closes writing file
+f.close()
