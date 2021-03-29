d06930eca28f49acb3a2898084588af8adba0800
lorivo <77751574+lorivo@users.noreply.github.com>
2021-03-29 14:15:18 -0500

    Add files via upload
    rough draft for writing full inventory, adjustments for formatting are needed

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -1,6 +1,7 @@
 # Lori Vo 1852113
 # Final Project for CIS 2348
 
+
 # reads given lists and adds to dictionary
 man_name = "ManufacturerList.csv"
 price_name = "PriceList.csv"
@@ -15,7 +16,7 @@ try:
 #        print(temp)
         data_dictionary[temp[0]] = temp[1:]
     man_file.close()
-    print(data_dictionary)
+#    print(data_dictionary)
 
     price_file = open(price_name)
     for line in price_file.readlines():
@@ -24,7 +25,7 @@ try:
 #        print(temp)
         data_dictionary[temp[0]].append(temp[1:])
     price_file.close()
-    print(data_dictionary)
+#    print(data_dictionary)
 
     SD_file = open(SD_name)
     for line in SD_file.readlines():
@@ -33,9 +34,21 @@ try:
 #        print(temp)
         data_dictionary[temp[0]].append(temp[1:])
     SD_file.close()
-    print(data_dictionary)
-
-reorganized_dictionary =
+#    print(data_dictionary)
 
 except:
     print("Error reading file or gathering data")
+
+# writes Full Inventory!!!
+of = open('FullInventory.csv', 'w')
+for n in data_dictionary:
+    value1 = n # ID
+    value2 = data_dictionary[n][0] #Manufacturer
+    value3 = data_dictionary[n][1] # Item type
+    value4 = data_dictionary[n][3] # Price
+    value5 = data_dictionary[n][4] # Service date
+    value6 = data_dictionary[n][2] # if damaged
+    of.write("{}, {}, {}, {}, {}, {}".format(value1, value2, value3, value4, value5, value6))
+    of.write('\n')
+#    print(n)
+of.close()
