9269be716e5d39844f40bdd371050068796c2080
lorivo <77751574+lorivo@users.noreply.github.com>
2021-03-29 13:43:01 -0500

    Add files via upload
    added lines that reads the other two given files and appends info to the dictionary

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -1,19 +1,41 @@
 # Lori Vo 1852113
 # Final Project for CIS 2348
 
-# reads manufacturer list
+# reads given lists and adds to dictionary
 man_name = "ManufacturerList.csv"
-data = []
+price_name = "PriceList.csv"
+SD_name = "ServiceDatesList.csv"
+
 data_dictionary = {}
 try:
     man_file = open(man_name)
     for line in man_file.readlines():
-        data.append(line.strip())
-        data_dictionary[line[0:7]] = line[8:]
-
+        line = line.strip()
+        temp = line.split(',')
+#        print(temp)
+        data_dictionary[temp[0]] = temp[1:]
     man_file.close()
-    print(data)
     print(data_dictionary)
+
+    price_file = open(price_name)
+    for line in price_file.readlines():
+        line = line.strip()
+        temp = line.split(',')
+#        print(temp)
+        data_dictionary[temp[0]].append(temp[1:])
+    price_file.close()
+    print(data_dictionary)
+
+    SD_file = open(SD_name)
+    for line in SD_file.readlines():
+        line = line.strip()
+        temp = line.split(',')
+#        print(temp)
+        data_dictionary[temp[0]].append(temp[1:])
+    SD_file.close()
+    print(data_dictionary)
+
+reorganized_dictionary =
 
 except:
     print("Error reading file or gathering data")
