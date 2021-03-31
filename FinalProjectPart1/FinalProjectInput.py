fcb21a5485116628801280184a9ea2f3ef95c831
lorivo <77751574+lorivo@users.noreply.github.com>
2021-03-31 14:18:58 -0500

    Add files via upload
    Completed part d of the output, which creates DamagedInventory.csv.

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -74,26 +74,50 @@ sorted_items = dict(sorted(data_dictionary.items(), key=lambda data_dictionary: 
 
 item_type = ""  # empty string for item type to change later in the code
 
-for n in sorted_items:
-    if item_type != sorted_items[n][1]:  # item type is different from before, initially ''
-        item_type = sorted_items[n][1]  # data dictionary's value of item type
+for u in sorted_items:
+    if item_type != sorted_items[u][1]:  # item type is different from before, initially ''
+        item_type = sorted_items[u][1]  # data dictionary's value of item type
         op = open(item_type+"Inventory.csv", 'w')  # makes new file to write in
-        value1 = n  # ID
-        value2 = data_dictionary[n][0]  # Manufacturer
-        value3 = data_dictionary[n][1]  # Item type
-        value4 = data_dictionary[n][3]  # Price
-        value5 = data_dictionary[n][4]  # Service date
-        value6 = data_dictionary[n][2]  # if damaged
+        value1 = u  # ID
+        value2 = data_dictionary[u][0]  # Manufacturer
+        value3 = data_dictionary[u][1]  # Item type
+        value4 = data_dictionary[u][3]  # Price
+        value5 = data_dictionary[u][4]  # Service date
+        value6 = data_dictionary[u][2]  # if damaged
         op.write("{}, {}, {}, {}, {}, {}".format(value1, value2, value3, value4, value5, value6))
         op.write('\n')
     else:  # if item type is same as before
         op = open(item_type + "Inventory.csv", 'a')   # appends to previous file instead of creating a new one
-        value1 = n  # ID
-        value2 = data_dictionary[n][0]  # Manufacturer
-        value3 = data_dictionary[n][1]  # Item type
-        value4 = data_dictionary[n][3]  # Price
-        value5 = data_dictionary[n][4]  # Service date
-        value6 = data_dictionary[n][2]  # if damaged
+        value1 = u  # ID
+        value2 = data_dictionary[u][0]  # Manufacturer
+        value3 = data_dictionary[u][1]  # Item type
+        value4 = data_dictionary[u][3]  # Price
+        value5 = data_dictionary[u][4]  # Service date
+        value6 = data_dictionary[u][2]  # if damaged
         op.write("{}, {}, {}, {}, {}, {}".format(value1, value2, value3, value4, value5, value6))
         op.write('\n')
     op.close()
+
+# writes damage inventory file!!
+# converts price information to int so I could sort from greatest to least
+for n in data_dictionary:
+    data_dictionary[n][3] = int(data_dictionary[n][3])
+# print(data_dictionary)
+
+# new dictionary that sorts from greatest to least
+# reverse is used bc the function without it makes it least to greatest
+price_sort = dict(sorted(data_dictionary.items(), key=lambda data_dictionary: data_dictionary[1][3], reverse=True))
+# print(price_sort)
+
+os = open('DamagedInventory.csv', 'w')
+
+for e in price_sort:
+    if price_sort[e][2] == 'damaged':
+        value1 = e  # ID
+        value2 = data_dictionary[e][0]  # Manufacturer
+        value3 = data_dictionary[e][1]  # Item type
+        value4 = data_dictionary[e][3]  # Price
+        value5 = data_dictionary[e][4]  # Service date
+        os.write("{}, {}, {}, {}, {}".format(value1, value2, value3, value4, value5))
+        os.write('\n')
+os.close()
